4b1f4af77157abcea354f97014e72b9e62d542c7
Roshan Sharma <sosh9855028591@gmail.com>
2025-02-20 08:41:09 -0500

    Updated feature up and down key css is remaining

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
-import Todo from './todo'
-const App = () => {
-  return (
-    <div><Todo/></div>
-  )
-}
+import React, { useState } from 'react';
+// import Todo from './Todo';
+import Todo from "./todo"
 
-export default App
+const App = () => {
+  // const [tasks, setTasks] = useState(["Eat Breakfast", "Take a shower", "Walk with dog"]);
+  // const [newTask, setNewTask] = useState("");
+
+  // function handleInputChange(event) {
+  //   setNewTask(event.target.value);
+  // }
+
+  // function addTask() {
+  //   if (newTask.trim() !== "") {
+  //     setTasks((t) => [...t, newTask]);
+  //     setNewTask("");
+  //   }
+  // }
+
+  // function deleteTask(index) {
+  //   const updatedTasks = tasks.filter((_, i) => i !== index);
+  //   setTasks(updatedTasks);
+  // }
+
+  // function moveTaskUp(index) {
+  //   if (index > 0) {
+  //     const updatedTasks = [...tasks];
+  //     [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
+  //     setTasks(updatedTasks);
+  //   }
+  // }
+
+  // function moveTaskDown(index) {
+  //   if (index < tasks.length - 1) {
+  //     const updatedTasks = [...tasks];
+  //     [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
+  //     setTasks(updatedTasks);
+  //   }
+  // }
+
+  return (
+    // <div className='bg-red-600'>
+    //   <div className='to-do-list'></div>
+    //   <h1>To-Do-List</h1>
+    //   <div>
+    //     <input type='text' placeholder='Enter a task' onChange={handleInputChange} value={newTask} />
+    //     <button onClick={addTask}>Add</button>
+    //   </div>
+    //   <ol>
+    //     {tasks.map((task, index) => (
+    //       <li key={index}>
+    //         <span>{task}</span>
+    //         <button onClick={() => deleteTask(index)}>Delete</button>
+    //         <button onClick={() => moveTaskUp(index)}>UP</button>
+    //         <button onClick={() => moveTaskDown(index)}>DOWN</button>
+    //       </li>
+    //     ))}
+    //   </ol>
+    // </div>
+    <>
+    <Todo/></>
+  );
+};
+
+export default App;
--- src/todo.jsx
+++ src/todo.jsx
@@ -1,58 +1,60 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-const todo = () => {
-  const[tasks, setTasks]=useState([ "Eat Breakfast","Take a shower", "walk with dog "]);
-  const[newTask, setNewTask] =useState("");
-  function handleInputChange(event){
-    setNewTask(event.target.value)
+const Todo = () => {
+  const [tasks, setTasks] = useState(["Eat Breakfast", "Take a shower", "Walk with dog"]);
+  const [newTask, setNewTask] = useState("");
 
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
   }
-  function addTask(){
-    if(newTask.trim()!=="")
-    {
-      setTasks(t=>[...t,newTask]);
+
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTasks(t => [...t, newTask]);
       setNewTask("");
     }
-
   }
-  function deleteTask(index){
-     const updatedTasks=tasks.filter((_, i)=>i!==index);
-     setTasks(updatedTasks); 
+
+  function deleteTask(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
   }
-  function moveTaskUp(index){
 
+  function moveTaskUp(index) {
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
+      setTasks(updatedTasks);
+    }
   }
-  function moveTaskDown(index){
 
+  function moveTaskDown(index) {
+    if (index < tasks.length - 1) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
+      setTasks(updatedTasks);
+    }
   }
-  return (<>
-  <div className='to-do-list'></div>
-  <h1>To-Do-List</h1>
-  <div>
-    <input  type='text' placeholder ='enter a task' onChange={handleInputChange}/>
-   <button onClick={addTask }>add</button>
-   
-  </div>
-  <ol>
-    {tasks.map((task,index)=>
-    <li key={index}>
-      <span>{task}</span>
-      <button  onClick={()=> deleteTask(index)}>
-        Delete
-      </button>
-      <button  onClick={()=> moveTaskUp(index)}>
-      UP
-      </button>
-   <   button  onClick={()=> moveTaskDown(index)}>
-   DOWN
-      </button>
 
-    </li>)}
-  </ol>
-  </>
+  return (
+    <div className='to-do-list'>
+      <h1>To-Do List</h1>
+      <div>
+        <input type='text' placeholder='Enter a task' value={newTask} onChange={handleInputChange} />
+        <button onClick={addTask}>Add</button>
+      </div>
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span>{task}</span>
+            <button onClick={() => deleteTask(index)}>Delete</button>
+            <button onClick={() => moveTaskUp(index)}>UP</button>
+            <button onClick={() => moveTaskDown(index)}>DOWN</button>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
 
-)
-
-}
-
-export default todo
+export default Todo;
